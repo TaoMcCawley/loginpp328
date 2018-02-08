a5b2fb81c5cd97429b73cd372c6c3d26ca57f863
Kianna Dyck <kdyck@mail.greenriver.edu>
2018-02-08 13:32:15 -0500

    added setters/getters. Added login function.

--- user.php
+++ user.php
@@ -19,4 +19,45 @@ class User
         $loggedIn = false;
     }
 
+    // methods
+    function login($username, $password) {
+        include 'users.php';
+        // check if $username and $password key value pair is inside defined array
+        foreach($loginArray as $user => $pw) {
+            if ($user == $username) {
+                // check if passwords match
+                if ($pw == $password) {
+                    echo "<p>$username is logged in.</p>";
+                    $this->loggedIn = true;
+                } else {
+                    echo "<p>Login error</p>";
+                }
+            }
+        }
+        // if user not found
+        echo "<p>Login error</p>";
+    }
+
+    function logout() {
+
+    }
+
+    // getters and setters
+    function getUsername()
+    {
+        return $this->username;
+    }
+
+    function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    function setPassword($password)
+    {
+        if(strlen($password) >= 6) {
+            $this->password = $password;
+        }
+
+    }
 }
